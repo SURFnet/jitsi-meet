5040174b3b771bc6d53f60397e1f449abc968c50
Erik Rozendaal <erik@deler.org>
2020-08-26 11:05:15 +0200

    Disable livestreaming and recording buttons to work around Jitsi bug
    Both are not enabled in the config but the buttons still show. Remove the
    buttons from the menu as a workaround.

--- interface_config.js
+++ interface_config.js
@@ -187,8 +187,8 @@ var interfaceConfig = {
      */
     TOOLBAR_BUTTONS: [
         'microphone', 'camera', 'closedcaptions', 'desktop', 'fullscreen',
-        'fodeviceselection', 'hangup', 'profile', 'info', 'chat', 'recording',
-        'livestreaming', 'etherpad', 'sharedvideo', 'settings', 'raisehand',
+        'fodeviceselection', 'hangup', 'profile', 'info', 'chat',
+        'etherpad', 'sharedvideo', 'settings', 'raisehand',
         'videoquality', 'filmstrip', 'invite', 'feedback', 'stats', 'shortcuts',
         'tileview', 'videobackgroundblur', 'download', 'help', 'mute-everyone'
     ],
